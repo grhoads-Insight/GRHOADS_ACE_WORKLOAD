aaa98d6f91eda565be47d14ebf2504f5a10a3b22
grhoads-insight <grant.rhoads@insight.com>
2022-08-30 10:27:29 -0400

    update naming with environment

--- variables.tf
+++ variables.tf
@@ -1,33 +1,34 @@
 variable "env" {
     description = "environment declaration"
+    default = "dev"
     #add a default? maybe change for each branch?
 }
 variable "app_service_plan_name" {
     description = "name for app service plan for workload"
-    default = "app-service-plan-main"
+    default = "app-service-plan-${var.env}"
 }
 
 variable "web_app_name" {
     description = "name for main web app"
-    default = "web-app-main"
+    default = "web-app-${var.env}"
 }
 
 variable "function_app_name" {
     description = "name for function app"
-    default = "function-app-main"
+    default = "function-app-${var.env}"
 }
 
 variable "log_space_name" {
     description = "name for log analytics workspace"
-    default = "log-space-main"
+    default = "log-space-${var.env}"
 }
 
 variable "cosmosDB-name" {
     description = "name for cosmosDB account"
-    default = "workload-cosmosdb"
+    default = "workload-cosmosdb-${var.env}"
 }
 
 variable "sql_database_name" {
     description = "name for sql database"
-    default = "workload-sqldatabase"
+    default = "workload-sqldatabase-${var.env}"
 }

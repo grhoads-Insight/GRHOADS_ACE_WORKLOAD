c2a2467c4970a401c1157cab73a461a89966c3d9
ChefRambo <grhoads11@gmail.com>
2022-08-31 13:10:44 -0400

    fixing var declration

--- variables.tf
+++ variables.tf
@@ -5,30 +5,30 @@ variable "env" {
 }
 variable "app_service_plan_name" {
     description = "name for app service plan for workload"
-    default = "app-service-plan-${var.env}"
+    default = "app-service-plan"
 }
 
 variable "web_app_name" {
     description = "name for main web app"
-    default = "web-app-"
+    default = "web-app"
 }
 
 variable "function_app_name" {
     description = "name for function app"
-    default = "function-app-${var.env}"
+    default = "function-app"
 }
 
 variable "log_space_name" {
     description = "name for log analytics workspace"
-    default = "log-space-${var.env}"
+    default = "log-space"
 }
 
 variable "cosmosDB-name" {
     description = "name for cosmosDB account"
-    default = "workload-cosmosdb-${var.env}"
+    default = "workload-cosmosdb"
 }
 
 variable "sql_database_name" {
     description = "name for sql database"
-    default = "workload-sqldatabase-${var.env}"
+    default = "workload-sqldatabase"
 }
